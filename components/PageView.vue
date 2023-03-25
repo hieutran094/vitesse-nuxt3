68af53cc7b3066553f9e3bf5fbbde37606214a96
Tran Hieu <hieutran98vn@gmail.com>
2023-03-25 22:30:47 +0700

    Update PageView.vue

--- components/PageView.vue
+++ components/PageView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const { data } = await useFetch('/api/pageview')
-
+const { data: customData } = await useFetch('https://sportlight-api.tranhieudev.com/api/v1/guest/product/now')
+console.log(customData)
 const time = useTimeAgo(() => data.value?.startAt || 0)
 </script>
 
@@ -9,5 +10,6 @@ const time = useTimeAgo(() => data.value?.startAt || 0)
     <span font-500 text-gray>{{ data?.pageview }}</span>
     page views since
     <span text-gray>{{ time }}</span>
+    <span class="text-xs">{{customData}}</span>
   </div>
 </template>
